532c84c058462ae43256f1cb508d8a297aed58f3
Galileo Guilherme <galileoguilherme@outlook.com>
2022-09-07 20:06:08 -0300

    Update

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -12,18 +12,14 @@ const Footer = () => {
   return (
     <div className="socials">
       <div className="github">
-        <a href="https://github.com/MayconDS " target="_blank">
-          <AiFillGithub /> MayconDS
+        <a href="https://github.com/GalileoGuilherme" target="_blank">
+          <AiFillGithub /> Galileo Guilherme
         </a>
       </div>
-      <div className="instagram">
-        <a href="https://www.instagram.com/mayconzzl/" target="_blank">
-          <AiOutlineInstagram /> mayconzzl
-        </a>
-      </div>
+      
       <div className="linkedin">
-        <a href="https://www.linkedin.com/in/maycondouglasss/" target="_blank">
-          <AiFillLinkedin /> maycondouglasss
+        <a href="https://www.linkedin.com/in/galileo-guilherme-01996693/" target="_blank">
+          <AiFillLinkedin /> Galileo Guilherme
         </a>
       </div>
     </div>
